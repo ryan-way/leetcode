37cd764735eabb92727eee19435df5f7c78ba0f6
ryan-way <ryan.keith.way@gmail.com>
2024-02-15 09:47:40 -0800

    Valid paretheses

--- src/problems/index.ts
+++ src/problems/index.ts
@@ -2,3 +2,4 @@ export * from "./easy/two-sum/solution";
 export * from "./easy/palindrome-number/solution";
 export * from "./easy/roman-to-integer/solution";
 export * from "./easy/longest-common-prefix/solution";
+export * from "./easy/valid-parentheses/solution";
---
+++ test/problems/easy/valid-parentheses.test.ts
@@ -0,0 +1,24 @@
+import { describe, expect, test } from "bun:test";
+import { isValid } from "../../../src/problems";
+
+describe("Valid Parentheses", () => {
+	test("()", () => {
+		expect(isValid("()")).toBe(true);
+	});
+
+	test("()[]{}", () => {
+		expect(isValid("()[]{}")).toBe(true);
+	});
+
+	test("(]", () => {
+		expect(isValid("(]")).toBe(false);
+	});
+
+	test("(", () => {
+		expect(isValid("(")).toBe(false);
+	});
+
+	test("]", () => {
+		expect(isValid("]")).toBe(false);
+	});
+});
