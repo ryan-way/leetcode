fa36910a31f6d166f809712e02197a136a436c91
ryan-way <ryan.keith.way@gmail.com>
2024-02-25 13:42:54 -0800

    longest palindrome substring

--- src/problems/index.ts
+++ src/problems/index.ts
@@ -4,3 +4,5 @@ export * from "./easy/roman-to-integer/solution";
 export * from "./easy/longest-common-prefix/solution";
 export * from "./easy/valid-parentheses/solution";
 export * from "./easy/merge-two-sorted-lists/solution";
+
+export * from "./medium/longest-palindromic-substring/solution";
---
+++ test/problems/medium/longest-palindromic-substring.test.ts
@@ -0,0 +1,17 @@
+import { describe, expect, test } from "bun:test";
+import { longestPalindrome } from "../../../src/problems";
+
+const cases = [
+	["babad", "bab"],
+	["cbbd", "bb"],
+	["a", "a"],
+];
+
+describe("Longest Palindromic Substring", () => {
+	test.each(cases)(
+		"longest palidrome in %s should be %s",
+		(input, expected) => {
+			expect(longestPalindrome(input)).toBe(expected);
+		},
+	);
+});
